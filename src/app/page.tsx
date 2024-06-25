765c292ca217f313961e13eb118632c3866574dd
ADITYA VERMA <adityav1304@gmail.com>
2024-06-25 15:12:23 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import { CountdownTimer } from '@/components/CountdownTimer/CountdownTimer';
+import '../styles/main.scss'
+
+const target = new Date('2024-06-25T15:15:00'); // Set the target date to June 25, 2024, at 3:10 PM
+
+const timerLabels = ['days', 'hours', 'minutes', 'seconds']
+
+export default function Home() { 
+
+  return (
+    <main className="container">
+      <section>
+          <h1>we&apos;re launching soon</h1>
+          <CountdownTimer targetDate={target.toISOString()} labels={timerLabels}/>
+      </section>
+    </main>
+  );
+}
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import './footer.scss'
+import { SocialIcon, SocialIconProps } from '../SocialIcon'
+
+interface SocialIconType {
+    socialicontype: SocialIconProps[]
+}
+
+export const FooterI = ({ socialicontype }: SocialIconType) => {
+    return (
+        <footer>
+            <div className="social">
+                <div className="social__group">
+                    <h2>SRMIST SIGKDD STUDENT CHAPTER</h2>
+                </div>
+            </div>
+        </footer>
+    )
+}
