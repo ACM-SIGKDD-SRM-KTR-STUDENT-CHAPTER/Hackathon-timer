22631a64d407648adf865978c4df155db68ff823
ADITYA VERMA <adityav1304@gmail.com>
2024-06-25 15:25:08 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { CountdownTimer } from '@/components/CountdownTimer/CountdownTimer';
 import '../styles/main.scss'
 
-const target = new Date('2024-06-25T15:25:00'); // Set the target date to June 25, 2024, at 3:10 PM
+const targetDateIST = new Date('2024-06-25T15:27:00+05:30'); // Set the target date to June 25, 2024, at 3:10 PM IST
 
-const timerLabels = ['days', 'hours', 'minutes', 'seconds']
+const timerLabels = ['days', 'hours', 'minutes', 'seconds'];
 
 export default function Home() { 
 
@@ -11,7 +11,7 @@ export default function Home() {
     <main className="container">
       <section>
           <h1>we&apos;re launching soon</h1>
-          <CountdownTimer targetDate={target.toISOString()} labels={timerLabels}/>
+          <CountdownTimer targetDate={targetDateIST.toISOString()} labels={timerLabels}/>
       </section>
     </main>
   );
