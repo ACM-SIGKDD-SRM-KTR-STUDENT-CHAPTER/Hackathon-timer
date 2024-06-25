f2992c5190013b145e6b97df698166ebd0c34912
ADITYA VERMA <adityav1304@gmail.com>
2024-06-25 15:30:07 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { CountdownTimer } from '@/components/CountdownTimer/CountdownTimer';
 import '../styles/main.scss'
 
 // Calculate the target date in IST
-const targetDateIST = new Date('2024-06-25T15:10:00+05:30');
+const targetDateIST = new Date('2024-06-25T15:35:00+05:30');
 
 // Convert IST to Washington, D.C. local time (EDT, UTC-4:00)
 const offsetISTtoEDT = (5 * 60 + 30) - 4 * 60; // Difference in minutes
