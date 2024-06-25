100f299bfa363135ce34292782ad76a88636db9a
ADITYA VERMA <adityav1304@gmail.com>
2024-06-25 16:06:30 +0530

    ui updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { CountdownTimer } from '@/components/CountdownTimer/CountdownTimer';
 import '../styles/main.scss';
 import moment from 'moment-timezone';
 
-// Set the target date to June 25, 2024, at 3:15 PM in IST timezone
-const target = moment.tz('2024-06-25 15:45:00', 'Asia/Kolkata');
+// add 3 sec to be accurate
+const target = moment.tz('2024-06-25 16:10:00', 'Asia/Kolkata');
 
 const timerLabels = ['days', 'hours', 'minutes', 'seconds'];
 
@@ -11,8 +11,7 @@ export default function Home() {
   return (
     <main className="container">
       <section>
-          <h1>we&apos;re launching soon</h1>
-          {/* Use target.toISOString() to pass the date in ISO format */}
+          <h1>HACKATHON TIMER</h1>
           <CountdownTimer targetDate={target.toISOString()} labels={timerLabels}/>
       </section>
     </main>
