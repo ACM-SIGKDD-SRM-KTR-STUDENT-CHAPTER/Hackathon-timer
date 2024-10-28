99278cbf26a1bf0f4e5064a79e06eb311cb05270
ADITYA VERMA <adityav1304@gmail.com>
2024-10-28 17:01:15 +0530

    A

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export const FooterI = () => {
         <footer>
             <div className="social">
                 <div className="social__group">
-                    <h2>SRMIST SIGKDD STUDENT CHAPTER</h2>
+                    <h2>SRMIST ACM SIGKDD STUDENT CHAPTER</h2>
                 </div>
             </div>
         </footer>
